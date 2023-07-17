722494600fc7069989ad692dc1f4808e8a5559b7
ASUMAN ODABAŞI <103738413+asumanodabasi@users.noreply.github.com>
2023-07-17 20:26:27 +0300

    Add files via upload

--- src/components/userComponent.js
+++ src/components/userComponent.js
@@ -1,15 +1,28 @@
+import { BaseLogger, ElasticLogger } from "../crossCutingConcerns/logging/logger.js"
 import User from "../models/user.js"
 import UserService from "../services/userService.js"
 
 console.log("user component yüklendi")
 
-let userService=new UserService()
+let log=new ElasticLogger()
+let userService=new UserService(log)
+
+
 
 let user1=new User(1,"Asu","Odabaşı","İstanbul")
+let user2=new User(1,"Melike","Odabaşı","İstanbul")
+
 
 let customer={id:1,firstName:"asu"}
 //prototyping : sonradan deger ekleme
 customer.lastName="Odabaşı"
 console.log(customer.lastName)
-userService.add()
+
+userService.add(user1)
+userService.add(user2)
+userService.list()
+console.log(userService.list())
+console.log(userService.getById(2))
+
+
 
--- src/services/userService.js
+++ src/services/userService.js
@@ -1,6 +1,19 @@
 export default class UserService{
+    constructor(loggerService){
+        this._users = []; // private property, only accessible within the class and its descendants
+        this.loggerService=loggerService
+    }
     add(user){
-        console.log("kullanıcı eklndi"+user)
+        this._users.push(user)
+        this.loggerService.log(user)
+    }
+
+    list(){
+       return this._users
+    }
+
+    getById(id){
+        return this._users.find(u=>u.id==id )
     }
 
 
